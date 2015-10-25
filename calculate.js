984cdb5179be4b0e80e59e136f639d1e6893cc10
[01;31m[K"name"[m[K: "Stephanie Hutson <[01;31m[K"email"[m[K: null>
2015-10-25 08:28:18 -0700

    add a how much is that portion and make it a littttttle prettier. made logo not yet viewable

--- calculate.js
+++ calculate.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
   console.log("I'm ready!")
-  $('body').prepend('<div class="header"><span><h2>Loading</h2></span><span><form id="vehicle"><input type="radio" name="automobile" value="car" checked>Car <input type="radio" name="automobile" value="suv">SUV <input type="radio" name="automobile" value="electric">Electric </form></span><span> <button class="refresh">Refresh Results</button></span></div>')
-  setTimeout(getEmissions, 3000)
-  $('button.refresh').click(refreshResults)
+  $('body').prepend('<div class="header"><span><h2>Loading</h2></span><span><form id="vehicle"><input type="radio" name="automobile" value="car" checked>Car <input type="radio" name="automobile" value="suv">SUV <input type="radio" name="automobile" value="electric">Electric <input type="radio" name="automobile" value="bus">Bus </form></span><span> <form class="refresh"><input type="Submit" value="Refresh Results"></form></span><span><form class="how-much"><input type="Submit" value="How Much is That?"></form></span></div><div class="how-much"></div>')
+  setTimeout(getEmissions, 2000)
+  $('form.refresh').submit(submitForm)
   $('form#vehicle').on("change", getVehicle)
+  $('form.how-much').submit(showHowMuch)
+  $('div.how-much').click(function(){
+    $(this).hide()
+  })
 })
 
 var getEmissions = function(vehicle){
@@ -12,13 +16,20 @@ var getEmissions = function(vehicle){
   var formattedDistance = formatDistance(distance)
   console.log(formattedDistance)
   emissionRate = emissionRateFor(vehicle)
-  if ( formattedDistance*emissionRate > 1000 ){
-    $('.header h2').html(Math.floor(formattedDistance* emissionRate)/1000 + " kilograms of CO2")
-  } else if (formattedDistance*emissionRate <= 1000 ){
-    $('.header h2').html(Math.floor(formattedDistance* emissionRate) + " grams of CO2")
+  var weight = emissionRate*formattedDistance
+  var volume = getVolume(weight)
+  if ( weight > 1000 ){
+    $('.header h2').html(Math.floor(weight)/1000 + " kilograms of CO2")
+  } else if (weight <= 1000 ){
+    $('.header h2').html(Math.floor(weight) + " grams of CO2")
   } else {
     $('.header h2').html("Select a journey and press refresh")
   }
+  $('div.how-much').html("<p>That's " + volume + " liters of CO2 in gas form</p><img src='imgs/logo48.png'/>")
+}
+var submitForm = function(e){
+  e.preventDefault()
+  refreshResults()
 }
 
 var refreshResults = function(vehicle){
@@ -27,7 +38,7 @@ var refreshResults = function(vehicle){
 }
 
 var formatDistance = function(distanceString){
-  var distanceNum = parseFloat(distanceString.replace(/\D/,"").replace(/^0/,"0."))
+  var distanceNum = parseFloat(distanceString.replace(/[ ]\D*/,""))
   return distanceNum
 }
 
@@ -43,6 +54,20 @@ var emissionRateFor = function(vehicle){
     return 513.8
   } else if (vehicle === "electric") {
     return 245.5
+  } else if (vehicle === "bus") {
+    return 69
   }
+}
 
+String.prototype.splice = function(index, string){
+  return this.substring(0,index) + string + this.substring(index, this.lenth)
+}
+
+var getVolume = function(weight){
+  return Math.floor(weight/0.00198)/1000
+}
+
+var showHowMuch = function(e){
+  e.preventDefault()
+  $('div.how-much').css("display", "table-cell")
 }
